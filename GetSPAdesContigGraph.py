9e8d5cd602584b162fd17d4b05b7a8ba459baadf
Ryan Wick <rrwick@gmail.com>
2015-11-20 16:09:16 +1100

    Finish program

--- GetSPAdesContigGraph.py
+++ GetSPAdesContigGraph.py
@@ -5,15 +5,15 @@
 
 # This file is part of GetSPAdesContigGraph.
 
-# GetSPAdesContigGraph is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free 
+# GetSPAdesContigGraph is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 
-# GetSPAdesContigGraph is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
+# GetSPAdesContigGraph is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
 
 # You should have received a copy of the GNU General Public License along with
 # GetSPAdesContigGraph.  If not, see <http:# www.gnu.org/licenses/>.
@@ -34,40 +34,24 @@ def main():
     startTime = datetime.datetime.now()
     args = getArguments()
 
-
     links = loadGraph(args.graph)
     contigs = loadContigs(args.contigs)
     paths = loadPaths(args.paths)
 
-    addGraphSegmentsToContigs(contigs, paths)
+    addLinksToContigs(contigs, paths, links)
 
+    output = []
     for contig in contigs:
-        print(contig.fullname)
-        print(contig.startingSegment)
-        print(contig.endingSegment)
-        print("")
-
-    quit()
-
-
-
-
-
-    # Create the output graph
-    outputLines = []
-
-
-    for contig in contigs:
-        outputLines.append(contig.getHeaderWithEdges())
-        outputLines += contig.sequenceLines
+        output.append(contig.getHeaderWithLinks())
+        output.append(contig.getSequenceWithLineBreaks())
 
     if args.output != "":
         outputFile = open(args.output, 'w')
-        for line in outputLines:
-            outputFile.write(line + '\n')
+        for line in output:
+            outputFile.write(line)
     else:
-        for line in outputLines:
-            print(line)
+        for line in output:
+            print(line, end='')
 
 
 
@@ -87,13 +71,16 @@ def getArguments():
 
 
 # This function takes a contig filename and returns a list of Contig objects.
+# It expects a file of just foward contigs, but it creates both forward and
+# reverse complement Contig objects.
 def loadContigs(contigFilename):
 
     contigs = []
+
     contigFile = open(contigFilename, 'r')
 
     name = ''
-    sequenceLines = []
+    sequence = ''
     for line in contigFile:
 
         strippedLine = line.strip()
@@ -107,21 +94,23 @@ def loadContigs(contigFilename):
 
             # If a contig is currently stored, save it now.
             if len(name) > 0:
-                contig = Contig(name, sequenceLines)
+                contig = Contig(name, sequence)
                 contigs.append(contig)
+                contigs.append(getReverseComplementContig(contig))
                 name = ''
-                sequenceLines = []
+                sequence = ''
 
             name = strippedLine[1:]
 
         # If not a header line, we assume this is a sequence line.
         else:
-            sequenceLines.append(strippedLine)
+            sequence += strippedLine
 
     # Save the last contig.
     if len(name) > 0:
-        contig = Contig(name, sequenceLines)
+        contig = Contig(name, sequence)
         contigs.append(contig)
+        contigs.append(getReverseComplementContig(contig))
 
     return contigs
 
@@ -129,7 +118,8 @@ def loadContigs(contigFilename):
 
 
 
-# This function takes a graph filename and returns a dictionary of the graph links.
+# This function takes a graph filename and returns a dictionary of the graph
+# links.
 # The dictionary key is the starting graph segment.
 # The dictionary value is a list of the ending graph segments.
 def loadGraph(graphFilename):
@@ -162,7 +152,8 @@ def loadGraph(graphFilename):
         if startRevComp not in links:
             links[start] = []
 
-        # If there are no outgoing links from this sequence, there's nothing else to do.
+        # If there are no outgoing links from this sequence, there's nothing
+        # else to do.
         if len(lineParts) < 2:
             continue
 
@@ -271,25 +262,107 @@ def getOppositeSequenceNumber(number):
 
 
 
-# This function uses the contents of the paths dictionary to add
-# first/last graph segment information to the contigs.
-def addGraphSegmentsToContigs(contigs, paths):
+# This function uses the contents of the paths dictionary to add link
+# information to the contigs.
+def addLinksToContigs(contigs, paths, links):
+
+    # First store the first and last graph segments for each contig.
     for contig in contigs:
         firstSegment, lastSegment = paths[contig.fullname]
         contig.addGraphSegments(firstSegment, lastSegment)
 
+    # Now for each contig, we take the last graph segment, find the segments
+    # that it leads to, and then find the contigs which start with that next
+    # segment.  These make up the links to the current contig.
+    for contig1 in contigs:
+        endingSegment = contig1.endingSegment
+        followingSegments = links[endingSegment]
+        linkedContigs = []
+        for followingSegment in followingSegments:
+            for contig2 in contigs:
+                startingSegment = contig2.startingSegment
+                if followingSegment == startingSegment:
+                    linkedContigs.append(contig2)
+        contig1.addLinkedContigs(linkedContigs)
 
+
+
+
+
+
+# This function takes a contig and returns its reverse complement contig.
+def getReverseComplementContig(contig):
+
+    revCompContigFullname = contig.fullname
+
+    # Add or remove the ' as necessary
+    if revCompContigFullname[-1] == "'":
+        revCompContigFullname = revCompContigFullname[0:-1]
+    else:
+        revCompContigFullname = revCompContigFullname + "'"
+
+    revCompSequence = getReverseComplement(contig.sequence)
+
+    return Contig(revCompContigFullname, revCompSequence)
+
+
+
+
+
+def getReverseComplement(forwardSequence):
+
+    reverseComplement = ""
+    for i in reversed(range(len(forwardSequence))):
+        base = forwardSequence[i]
+
+        if base == 'A': reverseComplement += 'T'
+        elif base == 'T': reverseComplement += 'A'
+        elif base == 'G': reverseComplement += 'C'
+        elif base == 'C': reverseComplement += 'G'
+        elif base == 'a': reverseComplement += 't'
+        elif base == 't': reverseComplement += 'a'
+        elif base == 'g': reverseComplement += 'c'
+        elif base == 'c': reverseComplement += 'g'
+        elif base == 'R': reverseComplement += 'Y'
+        elif base == 'Y': reverseComplement += 'R'
+        elif base == 'S': reverseComplement += 'S'
+        elif base == 'W': reverseComplement += 'W'
+        elif base == 'K': reverseComplement += 'M'
+        elif base == 'M': reverseComplement += 'K'
+        elif base == 'r': reverseComplement += 'y'
+        elif base == 'y': reverseComplement += 'r'
+        elif base == 's': reverseComplement += 's'
+        elif base == 'w': reverseComplement += 'w'
+        elif base == 'k': reverseComplement += 'm'
+        elif base == 'm': reverseComplement += 'k'
+        elif base == 'B': reverseComplement += 'V'
+        elif base == 'D': reverseComplement += 'H'
+        elif base == 'H': reverseComplement += 'D'
+        elif base == 'V': reverseComplement += 'B'
+        elif base == 'b': reverseComplement += 'v'
+        elif base == 'd': reverseComplement += 'h'
+        elif base == 'h': reverseComplement += 'd'
+        elif base == 'v': reverseComplement += 'b'
+        elif base == 'N': reverseComplement += 'N'
+        elif base == 'n': reverseComplement += 'n'
+        elif base == '.': reverseComplement += '.'
+        elif base == '-': reverseComplement += '-'
+        elif base == '?': reverseComplement += '?'
+        else: reverseComplement += 'N'
+
+    return reverseComplement
 
 
 
 # This class holds a contig: its name, sequence and length.
 class Contig:
 
-    def __init__(self, name, sequenceLines):
+    def __init__(self, name, sequence):
         self.fullname = name
         nameParts = name.split("_")
         self.number = int(nameParts[1])
-        self.sequenceLines = sequenceLines
+        self.sequence = sequence
+        self.linkedContigs = []
 
     def __str__(self):
         return self.fullname
@@ -301,23 +374,31 @@ class Contig:
         self.startingSegment = startingSegment
         self.endingSegment = endingSegment
 
-    def getHeaderWithEdges(self):
+    def addLinkedContigs(self, linkedContigs):
+        self.linkedContigs = linkedContigs
+
+    # This function produces a FASTG header for the contig, with links and a
+    # line break at the end.
+    def getHeaderWithLinks(self):
         headerWithEdges = '>' + self.fullname
 
-        # ADD THE EDGES HERE
+        if len(self.linkedContigs) > 0:
+            headerWithEdges += ":"
+            for linkedContig in self.linkedContigs:
+                headerWithEdges += linkedContig.fullname + ','
+            headerWithEdges = headerWithEdges[0:-1]
 
-        headerWithEdges += ";"
+        headerWithEdges += ";\n"
         return headerWithEdges
 
-
-
-
-# This class holds a contig: its name, sequence and length.
-class Path:
-
-    def __init__(self, name, pathLines):
-        self.firstSegment = "DSSDF" # TO DO: get the actual first segment
-        self.lastSegment = "DFGDFG" # TO DO: get the actual last segment
+    def getSequenceWithLineBreaks(self):
+        sequenceRemaining = self.sequence
+        returnSequence = ""
+        while len(sequenceRemaining) > 60:
+            returnSequence += sequenceRemaining[0:60] + '\n'
+            sequenceRemaining = sequenceRemaining[60:]
+        returnSequence += sequenceRemaining + '\n'
+        return returnSequence
 
 
 
